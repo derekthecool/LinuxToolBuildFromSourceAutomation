707cd3ba2d33f19ee83aee1efec6bcf8b1521e6f
derekthecool <derekthecool@gmail.com>
2021-09-07 10:01:04 -0600

    Fixing big problem with updating git submodules
    Using 'git pull --recurse-submodules' is good for cloning this main
    repo, but it is not good for updating the git submodules.
    
    Use 'git submodule update' in the build_all.sh script to always pull the
    latest updates and then build all.

--- build_all.sh
+++ build_all.sh
@@ -3,15 +3,19 @@
 # TODO: check for required system dependencies
 
 # Update all git submodules
-git pull --recurse-submodules
+git submodule update --recursive --remote
 
 # Variable to keep track of how many builds we have done
 buildCount=0
 
+# Color codes for printing
+normal="\e[39m"
+green="\e[92m"
+
 # Build all submodule repositories in this project
 for repo in */; do
   # Print that we are starting with this build
-  echo "----------------${repo} build starting----------------"
+  echo -e "$green----------------${repo} build starting----------------$normal"
 
   # Enter build directory
   cd "$repo" || exit
@@ -26,8 +30,8 @@ for repo in */; do
   cd ..
 
   # Print that we are finished with this build
-  echo -e "----------------${repo} build complete----------------\n"
+  echo -e "$green----------------${repo} build complete----------------$normal\n"
 done
 
 # Specify how many builds were completed successfully
-echo -e "\n\nBuilds completed: $buildCount"
+echo -e "\n\nBuilds completed: $green$buildCount$normal"
